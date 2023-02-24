42ece30ce1e39691a2ccf56f4316771e8bbd340b
Seamus <Seamus_Gu@sz.ctil.com>
2023-02-24 17:44:20 +0800

    IC-2022.2.3 <Seamus Gu@SZO-22-P10 Create easyJavadoc.xml, ide.general.xml, advancedSettings.xml, diff.xml, shared-indexes.xml, Default.xml, find.xml, ignored-suggested-plugins.xml, DontShowAgainFeedbackService.xml, filetypes.xml, keymap.xml, Java.xml, editor-font.xml, colors.scheme.xml, templates.xml, laf.xml, GrepConsole.xml, Windows.xml, Class.java, jdk.table.xml, MyBatisCodeHelper.xml, dbnavigator.xml, vcs.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+
+/**
+ * ${description}
+ * @author ${USER}
+ * @date ${YEAR}/${MONTH}/${DAY} ${HOUR}:${MINUTE}
+ **/
+public class ${NAME} {
+}
